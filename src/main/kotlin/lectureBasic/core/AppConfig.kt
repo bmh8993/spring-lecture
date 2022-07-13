18db389b84788f9e41644ac4ddc956066bdc4f82
bmh8993 <bmh9175@gmail.com>
2022-07-13 14:02:26 +0900

    AppConfig를 리팩토링한다.
    - 변수에 객체를 할당한 후에 해당 변수를 파라미터로 넘겼다.
    - 이렇게 하면 역할과 구현이 잘 보인다.
    - 붕복을 제거할 수도 있다.

--- src/main/kotlin/lectureBasic/core/AppConfig.kt
+++ src/main/kotlin/lectureBasic/core/AppConfig.kt
@@ -1,7 +1,9 @@
 package lectureBasic.core
 
 import MemberServiceImpl
+import lectureBasic.core.discount.DiscountPolicy
 import lectureBasic.core.discount.FixDiscountPolicy
+import lectureBasic.core.member.MemberRepository
 import lectureBasic.core.member.MemberService
 import lectureBasic.core.member.repository.MemoryMemberRepository
 import lectureBasic.core.order.OrderService
@@ -9,11 +11,22 @@ import lectureBasic.core.order.service.OrderServiceImpl
 
 class AppConfig {
 
+    /**
+     * before
+     * - 메서드의 파라미터로 객체를 직접 생성했다. 그렇다보니 중복이 발생했다. ex. MemoryMemberRepository
+     * - 그렇기때문에 구현을 바꾸게 되면 하나씩 전체를 바꿔주어야했다.(=구현이 잘 보이지 않는다.)
+     *
+     * after
+     * - 변수에 객체를 할당하여 해당 변수를 파라미터로 넘겨주었다. 구현을 바꾸게 된다면 해당 객체만 교체하면 된다.
+     */
+    private val memberRepository: MemberRepository = MemoryMemberRepository()
+    private val discountPolicy: DiscountPolicy = FixDiscountPolicy()
+
     fun memberService(): MemberService {
-        return MemberServiceImpl(MemoryMemberRepository())
+        return MemberServiceImpl(memberRepository)
     }
 
     fun orderService(): OrderService {
-        return OrderServiceImpl(MemoryMemberRepository(), FixDiscountPolicy())
+        return OrderServiceImpl(memberRepository, discountPolicy)
     }
 }
