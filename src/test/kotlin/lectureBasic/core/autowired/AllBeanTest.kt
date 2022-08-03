30f8c083daee45491dc3cb72652c8d3abbd2ac5f
bmh8993 <bmh9175@gmail.com>
2022-08-03 14:49:16 +0900

    AutoAppConfig도ComponentClasses로 추가한 이후 정상 동작확인

--- src/test/kotlin/lectureBasic/core/autowired/AllBeanTest.kt
+++ src/test/kotlin/lectureBasic/core/autowired/AllBeanTest.kt
@@ -1,5 +1,6 @@
 package lectureBasic.core.autowired
 
+import lectureBasic.core.AutoAppConfig
 import lectureBasic.core.discount.DiscountPolicy
 import lectureBasic.core.member.Grade
 import lectureBasic.core.member.Member
@@ -13,12 +14,12 @@ class AllBeanTest {
     @Test
     fun findAllBean() {
 
-        val ac: ApplicationContext = AnnotationConfigApplicationContext(DiscountService::class.java)
+        val ac: ApplicationContext = AnnotationConfigApplicationContext(AutoAppConfig::class.java, DiscountService::class.java)
         val discountService: DiscountService = ac.getBean(DiscountService::class.java)
 
         val member = Member(1L, "userA", Grade.VIP)
 
-        val discountPrice = discountService.discount(member, 10000, "FixDiscountPolicy")
+        val discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy")
 
         Assertions.assertThat(discountService).isInstanceOf(DiscountService::class.java)
         Assertions.assertThat(discountPrice).isEqualTo(1000)
@@ -36,8 +37,12 @@ class AllBeanTest {
                 println(this.policyMap)
             }
 
-            fun discount(member: Member, price: Int, discountCode: String): Int {
-                val discountPolicy: DiscountPolicy = policyMap[discountCode] ?: return 0
+            fun discount(member: Member, price: Int, discountPolicyCode: String): Int {
+                if (discountPolicyCode !in this.policyMap.keys) {
+                    throw Exception("존재하지 않는 discountPolicyCode 입니다.")
+                }
+
+                val discountPolicy: DiscountPolicy = policyMap[discountPolicyCode]!!
                 return discountPolicy.discount(member, price)
             }
         }
