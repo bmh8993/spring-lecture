b56cc35a47b5366350d36769f326921d3ffb6c77
bmh8993 <bmh9175@gmail.com>
2022-11-19 22:16:09 +0900

    provider를 사용하여 싱글톤 스코프와 리퀘스트 스코프를 같이 사용했을 때에 문제를 해결하라

--- src/main/kotlin/lectureBasic/core/web/LogDemoController.kt
+++ src/main/kotlin/lectureBasic/core/web/LogDemoController.kt
@@ -2,6 +2,7 @@ package lectureBasic.core.web
 
 import lectureBasic.core.common.MyLogger
 import lectureBasic.core.logdemo.LogDemoService
+import org.springframework.beans.factory.ObjectProvider
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -10,13 +11,14 @@ import javax.servlet.http.HttpServletRequest
 @Controller
 class LogDemoController(
     private val logDemoService: LogDemoService,
-    private val myLogger: MyLogger
+    private val myLoggerProvider: ObjectProvider<MyLogger>
 ) {
-
     @RequestMapping("log-demo")
     @ResponseBody
     fun logDemo(request: HttpServletRequest): String {
         val requestUrl = request.requestURL.toString()
+        val myLogger = myLoggerProvider.getObject()
+
         myLogger.setRequestUrl(requestUrl)
 
         myLogger.log("controller test")
