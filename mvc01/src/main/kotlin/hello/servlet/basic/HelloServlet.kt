387f2ce4e0180eb8baad0422a2f96a4d64db0e82
bmh8993 <bmh9175@gmail.com>
2023-02-27 20:29:32 +0900

    서블릿 기본 사용법 익히기

--- mvc01/src/main/kotlin/hello/servlet/basic/HelloServlet.kt
+++ mvc01/src/main/kotlin/hello/servlet/basic/HelloServlet.kt
@@ -7,7 +7,18 @@ import javax.servlet.http.HttpServletResponse
 
 @WebServlet(name = "helloServlet", urlPatterns = ["/hello"])
 class HelloServlet : HttpServlet() {
-    override fun service(req: HttpServletRequest?, resp: HttpServletResponse?) {
+    override fun service(request: HttpServletRequest?, response: HttpServletResponse?) {
         println("HelloServlet.service")
+        println("request = $request") // HttpServletRequest, HttpServletResponse는 인터페이스이고, was별로 해당 인터페이스를 사용해서 구현체를 작성한다.
+        println("response = $response")
+
+        // 쿼리 파라미터 조회
+        val username: String? = request?.getParameter("username")
+        println("username = $username")
+
+        // response에 응답 추가하기
+        response?.contentType = "text/plain"
+        response?.characterEncoding = "utf-8"
+        response?.writer?.write("hello $username")
     }
 }
