ccef27ecb5a3d0f39a4f8ac1a0eb0bd59fdfe049
bmh8993 <bmh9175@gmail.com>
2023-03-22 10:42:31 +0900

    application/json 방식 요청을 처리하라

--- mvc01/src/main/kotlin/hello/servlet/basic/RequestBodyJsonServlet.kt
+++ mvc01/src/main/kotlin/hello/servlet/basic/RequestBodyJsonServlet.kt
@@ -1,5 +1,10 @@
 package hello.servlet.basic
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.SingletonSupport
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.util.StreamUtils
 import java.nio.charset.StandardCharsets
 import javax.servlet.ServletInputStream
@@ -8,16 +13,20 @@ import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-@WebServlet(name = "requestBodyStringServlet", urlPatterns = ["/request-body-string"])
-class RequestBodyStringServlet : HttpServlet() {
-    override fun service(request: HttpServletRequest, response: HttpServletResponse) {
-        // body의 내용을 바이트로 받음
-        val inputStream: ServletInputStream = request.inputStream
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = ["/request-body-json"])
+class RequestBodyJsonServlet : HttpServlet() {
 
-        // 바이트를 문자열로 변환
+    private val mapper = jacksonObjectMapper()
+
+    override fun service(request: HttpServletRequest, response: HttpServletResponse) {
+        val inputStream: ServletInputStream = request.inputStream
         val messageBody: String = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8)
 
         println("messageBody = $messageBody")
+
+        val helloData: HelloData = mapper.readValue(messageBody, HelloData::class.java)
+
+        println("helloData = $helloData")
 
         response.writer.write("ok")
     }
